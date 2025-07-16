a0625e716a9d7f468f7e232bb29da5dcabfc1b67
Stevvan93 <eddesandell@gmail.com>
2025-07-16 21:57:13 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,8 @@
-
 export default function Contact() {
   return (
-    <div className="p-6 bg-white min-h-screen text-center">
-      <h1 className="text-3xl text-pink-700 font-bold mb-4">Kontakt</h1>
-      <p>Skicka meddelande till <strong>bettingparadise1@gmail.com</strong></p>
+    <div className="p-6 text-pink-600">
+      <h1 className="text-3xl font-bold mb-4">Kontakta oss</h1>
+      <p>Maila oss på <a href="mailto:bettingparadise1@gmail.com" className="underline">bettingparadise1@gmail.com</a></p>
     </div>
   );
 }
