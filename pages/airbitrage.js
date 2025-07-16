bbdd92c5a940acdbbdd102571cebe1f4ca881aa4
Stevvan93 <eddesandell@gmail.com>
2025-07-16 21:56:27 +0200

    Update airbitrage.js

--- pages/airbitrage.js
+++ pages/airbitrage.js
@@ -1,9 +1,8 @@
-
 export default function Airbitrage() {
   return (
-    <div className="p-6 bg-white min-h-screen text-center">
-      <h1 className="text-3xl text-pink-700 font-bold mb-4">Airbitrage</h1>
-      <p>Vårt verktyg för att hitta riskfria spel. (Demo-version just nu)</p>
+    <div className="p-6 text-pink-600">
+      <h1 className="text-3xl font-bold mb-4">Airbitrageverktyg</h1>
+      <p>Verktyget kommer snart. Håll utkik!</p>
     </div>
   );
 }
